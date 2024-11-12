7b80e2cffe3efde64bf897cae202e08f54bcc2a5
bidi <khalil.bidi@student.vinci.be>
2024-11-12 21:20:06 +0100

    add (singleMinute) :when given 1, return 'YOOO'

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -2,7 +2,11 @@ import { Main } from "../src/app";
 
 describe("A suite of test for the berlinClock function of the main class", function() {
     let main = new Main();
-    it("idniahfizheifizefhi")
+    it("ClockSingleMinute should return YOOO when given 1", function() {
+        const result = main.ClockSingleMinute(1);
+
+        expect(result).toBe(1);
+    });
 
 
 })
